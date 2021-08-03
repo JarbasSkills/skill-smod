d938314cc9119dce18325e75060b01b83c01cd04
jarbasal <jarbasai@mailfence.com>
2021-08-03 18:22:40 +0100

    bump ovos version

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 from pyvod import Collection
 from os.path import join, dirname, basename
-from ovos_utils.skills.templates.video_collection import VideoCollectionSkill
 from mycroft.skills.core import intent_file_handler
 from pyvod import Collection, Media
 from os.path import join, dirname, basename
-from ovos_utils.playback import CPSMatchType, CPSPlayback, CPSMatchConfidence
+from ovos_workshop.frameworks.playback import CPSMatchType, CPSPlayback, \
+    CPSMatchConfidence
+from ovos_workshop.skills.video_collection import VideoCollectionSkill
 
 
 class SMODSkill(VideoCollectionSkill):
