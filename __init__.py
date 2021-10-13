4cf85da557511ec184348ffb1e0282c135d832cf
jarbasal <jarbasai@mailfence.com>
2021-10-13 22:58:50 +0100

    ocp playlists

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 from os.path import join, dirname, basename
+import random
 
 from mycroft.skills.core import intent_file_handler
 from ovos_plugin_common_play.ocp import MediaType, PlaybackType
+from ovos_workshop.skills.common_play import ocp_search
 from ovos_workshop.skills.video_collection import VideoCollectionSkill
 from pyvod import Collection
 
@@ -12,28 +14,22 @@ class SMODSkill(VideoCollectionSkill):
         super().__init__("SMOD")
         self.message_namespace = basename(dirname(__file__)) + ".jarbasskills"
         self.default_image = join(dirname(__file__), "ui", "smod_logo.png")
-        self.skill_logo = join(dirname(__file__), "ui", "smod_icon.png")
         self.skill_icon = join(dirname(__file__), "ui", "smod_icon.png")
         self.default_bg = join(dirname(__file__), "ui", "smod_logo.png")
         self.supported_media = [MediaType.GENERIC,
                                 MediaType.VIDEO,
                                 MediaType.MUSIC]
-
         path = join(dirname(__file__), "res", "smod.jsondb")
         # load video catalog
-        self.media_collection = Collection("smod", logo=self.skill_logo,
+        self.media_collection = Collection("smod", logo=self.skill_icon,
                                            db_path=path)
-        self.media_type = MediaType.MUSIC
-        self.playback_type = PlaybackType.AUDIO
-
-    def get_intro_message(self):
-        self.speak_dialog("intro")
+        self.n_mixes = 3
 
     @intent_file_handler('home.intent')
     def handle_homescreen_utterance(self, message):
         self.handle_homescreen(message)
 
-    def match_media_type(self, phrase, media_type):
+    def get_base_score(self, phrase, media_type):
         score = 0
 
         if self.voc_match(phrase, "music") or media_type == MediaType.MUSIC:
@@ -54,6 +50,36 @@ class SMODSkill(VideoCollectionSkill):
             score += 80
 
         return score
+
+    @ocp_search()
+    def ocp_smod(self, phrase, media_type):
+        score = self.get_base_score(phrase, media_type)
+        if score < 50:
+            return
+        pl = [
+            {
+                "match_confidence": score,
+                "media_type": MediaType.MUSIC,
+                "uri": "youtube//" + entry["url"],
+                "playback": PlaybackType.AUDIO,
+                "image": entry["logo"],
+                "bg_image": self.default_bg,
+                "skill_icon": self.skill_icon,
+                "title": entry["title"]
+            } for entry in self.videos  # VideoCollectionSkill property
+        ]
+        for i in range(self.n_mixes):
+            random.shuffle(pl)
+            yield {
+                "match_confidence": score,
+                "media_type": MediaType.MUSIC,
+                "playlist": pl[:50],
+                "playback": PlaybackType.AUDIO,
+                "skill_icon": self.skill_icon,
+                "image": self.skill_icon,
+                "bg_image": self.default_bg,
+                "title": f"Stoned Meadow of Doom (Mix {i + 1})"
+            }
 
 
 def create_skill():
