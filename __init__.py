20106100b79437c5d634f28b3a3dfd5575a2797a
jarbasai <jarbasai@mailfence.com>
2022-02-05 17:08:26 +0000

    skill settings initialize + ocp

--- __init__.py
+++ __init__.py
@@ -2,9 +2,10 @@ import random
 from os.path import join, dirname
 
 from ovos_plugin_common_play.ocp import MediaType, PlaybackType
+from ovos_utils.log import LOG
 from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill, \
     ocp_search, ocp_featured_media
-from youtube_archivist import YoutubeArchivist
+from youtube_archivist import YoutubeMonitor
 
 
 class SMODSkill(OVOSCommonPlaybackSkill):
@@ -16,31 +17,21 @@ class SMODSkill(OVOSCommonPlaybackSkill):
                                 MediaType.VIDEO,
                                 MediaType.MUSIC]
         # load video catalog
-        self.archive = YoutubeArchivist("smod", blacklisted_kwords=["subscribe"])
+        self.archive = YoutubeMonitor("smod", logger=LOG, blacklisted_kwords=["subscribe"])
         self.n_mixes = 5
 
     def initialize(self):
-        if len(self.archive.db):
-            # update db sometime in the next 12 hours, randomized to avoid a huge network load every boot
-            # (every skill updating at same time)
-            self.schedule_event(self._scheduled_update, random.randint(3600, 12 * 3600))
-        else:
-            # no database, sync right away
-            self.schedule_event(self._scheduled_update, 5)
-
-    def _scheduled_update(self):
-        self.update_db()
-        self.schedule_event(self._scheduled_update, random.randint(3600, 12 * 3600))  # every 6 hours
-
-    def update_db(self):
+        bootstrap = f"https://raw.githubusercontent.com/OpenJarbas/streamindex/main/{self.archive.db.name}.json"
+        self.archive.bootstrap_from_url(bootstrap)
+        self.archive.setDaemon(True)
+        self.archive.start()
         urls = [
             "https://www.youtube.com/c/666MrDoom/videos",
             "https://www.youtube.com/c/StonedMeadowOfDoom2/videos",
             "https://www.youtube.com/c/StonedMeadowOfDoom1993/videos",
             "https://www.youtube.com/channel/UCWf14ZX2SZV4hF-hDVKdGvQ/videos"]
         for url in urls:
-            self.archive.archive(url)
-        self.archive.remove_unavailable()  # check if video is still available
+            self.archive.monitor(url)
 
     def match_skill(self, phrase, media_type):
         score = 0
@@ -81,18 +72,18 @@ class SMODSkill(OVOSCommonPlaybackSkill):
     @ocp_featured_media()
     def featured_media(self, num_entries=250):
         return [
-            {
-                "match_confidence": 90,
-                "media_type": MediaType.MUSIC,
-                "uri": "youtube//" + entry["url"],
-                "playback": PlaybackType.AUDIO,
-                "image": entry["thumbnail"],
-                "bg_image": self.default_bg,
-                "skill_icon": self.skill_icon,
-                "skill_id": self.skill_id,
-                "title": entry["title"]
-            } for entry in self.archive.sorted_entries()
-        ][:num_entries]
+                   {
+                       "match_confidence": 90,
+                       "media_type": MediaType.MUSIC,
+                       "uri": "youtube//" + entry["url"],
+                       "playback": PlaybackType.AUDIO,
+                       "image": entry["thumbnail"],
+                       "bg_image": self.default_bg,
+                       "skill_icon": self.skill_icon,
+                       "skill_id": self.skill_id,
+                       "title": entry["title"]
+                   } for entry in self.archive.sorted_entries()
+               ][:num_entries]
 
 
 def create_skill():
